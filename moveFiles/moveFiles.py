0fcb40ce51549340db94ead956a3f20ad538c6d9
eGangotri <egangotritrust@gmail.com>
2023-09-25 12:34:28 +0530

    .

--- moveFiles/moveFiles.py
+++ moveFiles/moveFiles.py
@@ -3,7 +3,8 @@ import shutil
 from pdfs import pdfNames
 from svc import getPdfCount
 
-def fileMover(src_dir,dest_dir,file_name):
+
+def fileMover(src_dir, dest_dir, file_name):
     # Check if the file already exists in the destination directory
     if os.path.exists(os.path.join(dest_dir, file_name)):
         # If the file exists, append a number to the file name to make it unique
@@ -13,29 +14,39 @@ def fileMover(src_dir,dest_dir,file_name):
         file_name = f"{file_name}_{i}"
 
     # Move the file to the destination directory
-    shutil.move(os.path.join(src_dir, file_name), os.path.join(dest_dir, file_name))
+    shutil.move(os.path.join(src_dir, file_name),
+                os.path.join(dest_dir, file_name))
 
-src_dir = 'D:\\_Treasures62\\_freeze\\panini'
-dest_dir = 'D:\\_Treasures62\\_data\\panini'
-_pdfNames = pdfNames
 
-def processData(src_dir,dest_dir,pdfNames):
-    pdfNamesAsList = pdfNames.split(',')
+src_dir = 'C:\\tmp\\_freeze\\test'
+dest_dir = 'C:\\tmp\\_data\\test'
+
+def processData(src_dir, dest_dir, pdfNames=""):
+    pdfNamesAsList = []
+    if (pdfNames == ""):
+        # get list of all files in src_dir
+        for file in os.listdir(src_dir):
+            pdfNamesAsList.append(file) # add file to list
+
+    else:
+        pdfNamesAsList = pdfNames.split(',')
+    print(f"Shall move {pdfNamesAsList}")
     print(f"Shall move {len(pdfNamesAsList)}")
     print(f"src_dir pdf count before {getPdfCount(src_dir)}")
     print(f"dest_dir pdf count before {getPdfCount(dest_dir)}")
 
     for pdf in pdfNamesAsList:
         pdf = pdf.strip()
-        fileMover(src_dir,dest_dir,pdf)
+        fileMover(src_dir, dest_dir, pdf)
         print(pdf)
     # how find count of pdfs in folfer stored in dest_dir
-    
+
     print(f"Shall move {len(pdfNamesAsList)}")
     print(f"src_dir pdf count after {getPdfCount(src_dir)}")
     print(f"dest_dir pdf count after {getPdfCount(dest_dir)}")
 
+
 if __name__ == "__main__":
-    processData(src_dir,dest_dir,_pdfNames)
+    processData(src_dir, dest_dir, "")
 
 # python moveFiles/moveFiles.py
