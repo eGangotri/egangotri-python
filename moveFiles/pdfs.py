18c2269d179bf4ebeb1ba9e2f93f998de6ab2134
egangotritrust@gmail.com <egangotritrust@gmail.com>
2025-02-05 17:48:50 +0700

    first and last

--- moveFiles/pdfs.py
+++ moveFiles/pdfs.py
@@ -1,47 +1,78 @@
 pdfNames = """
-1.txt,2.txt
+Arya Parv Paddati Ajmeir - Arya Sahitya Mandal Limited.pdf,Atharva Vediya Pruthvi Sukth(Matrubhumi Ka Mahatwa by Shri Swami Vedanand Teertha Ajmeir - Arya Sahitya Mandal Limited.pdf,Dayand Darshan (Philosaphy Of Swami Dayananda)  by Dr.Ved Prakash Gupta 1973 Meerut - Prakashan Pratishtan.pdf,Dayand Darshan (Philosaphy Of Swami Dayananda) by Dr.Ved Prakash Gupta 1975 Meerut - Satyartha Prakashan Samsthan.pdf,Aadima Satyartha Prakash Aur Arya Samaj Ke Sddhant by Shraddhanand Sanyasi 1918 Karnal - Pandit Vishnu Mitra Sharma.pdf,Alankar Tatha Gurukul Samachaar Kangadi - Snatak Gurukul Mandal.pdf,Amar Hutatma Ed by Murari 1976 Delhi - Arya Pratinidhi Sabha.pdf,Political Testament Of Swami Shradhanand Punjab - Shri Virendra.pdf,Sankshipta Manusmruthi by Munshiram 1909 Kangadi - Saddharma Pracharak Yantralay.pdf,Achar Anachar Aur Chut Chat by Munshi Ram Jignyasu 1916 Kangdi - Pracharak Pustak Bhandar.pdf,Manav Dharma Shastra Tatha Sasana Paddhati by Munshi Ram Jignyasu 1917 Kangadi - Pracharak Pustak Bhandar.pdf,Saraswati Sammelan Ka Chaturtha Varhika Vruttant 1908 Gurukul - Mushiram Jignyasu.pdf,Terahva Hindi Sahitya Sammelan Kanpur Ka Karya Vivaran Part 1 1923 Cawnpore - Hindi Sahitya Sammelan.pdf,Kathakam (Die Samhita Der Katha Cakha) by Leopold Von Schroeder 1900 - Anon.pdf,Maharshi Dayanand Ka Jeevan Charitra by Indra Vidya Vachaspati 1950 Delhi - Vijay Pustak Bhandar.pdf,Maharshi Dayanand Praneeta Raj Dharma by Lakshmi Datta Deekshit 1950 - Arya Pratinidhi Sabha.pdf,Mughal Samrajya Ka Kshya Aur Uska Karan Patrt 3 and 4 by Indra Vidya Vachaspati 1936 Mumbai - Hindi Grath Ratnakar Karyalay.pdf,Samrat Raghu by Indra Vidyavachaspati 1950 Delhi - Vijay Pustak Bhandar.pdf,Sant Jeevani by Girija Kumar Ghosh 1918 Kangadi - Sahitya Parishad Karyalay.pdf,Swarajya Aur Charitra Nirman by Indra Vidya vachaspati Delhi - Vachaspati Pustak Bhandar.pdf,Samaved Adhyatmik Muni Bhashya by Swami Brahmamuni Parivrajak Vidya Martand 1972 New Delhi - Arya Pratinidhi Sabha.pdf,Sankhya Darshanam with Brahma Muni Bhashya by Swami Brahma Muni Parivrajak Delhi - Arya Pratinidhi Sabha.pdf,Vaisheshik Darshan with Brahma Muni Bhashya by Swami Brahma Muni Parivrajak 1962 Badowda - Arya Kumar Maha Sabha.pdf,Vedadhyayan Praveshika by Swami Brahma Muni Parivrajak Vidyamartand 1968 New Delhi - Sarvadeshika Arya Pratinidhi Sabha.pdf,Abhyas Aur Viragya by Brahma Muni Parivrajak Vidyamartand 1961 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,Arsha Yoga Pradeepika by Swami Brahma Muni Parivrajak 1949 Dehradun - Pandit Thakur Datt Sharma Trust.pdf,Atharva Vediya Mantra vidya by Swami Brahmamuni Parivrajak 1941 New Delhi - Dayananda Samsthan.pdf,Atharvved Muni Bhashya Part 3 by Swami Brahmamuni Parivrajak Vidyamartand 1974 new Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,Gurudatt Lekhavali by Gurudatta Ji Vidyarthi Translated by Sant Ram 1918 Lahore - Arya Pustakalay.pdf,Samaved Adhyatmik Muni Bhashya by Swami Brahmamuni Parivrajak Vidyamartand 1970 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,Atharva Ved Muni Bhashya by Swami Brahmamuni Parivrajak Vidyamartand 1974 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,Avvyayartha Nibadhanam by Swami Brahmamuni Parivrajak Vidyamartand 1967 Rohatak - Haryana Sahitya Samsthan.pdf,Bala Samskruti Sudha by Swami Brahma Muni Parivrajak Vidyamartand 1961 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,Mahabharat Siksha Sudha by Swami Brahma Muni 1953 Ajmeir - Arya Sahitya Mandal.pdf,Mantrom ke Rushi by Swami Brahma Muni Parivrajak Vidyamartand 1974 Ajmeir - Arya Sahitya Mandal.pdf,Nyaya Darshan with Vatsayana Bhashya and Hindi Translation by Swami Brahma Muni Parivrajak Vidya Martand 1968 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,Samasudha by Swami Brahma Muni Parivrajak Vidya Martand 1963 Delhi - Govindaram Hasanand.pdf,Sankhya Darshanam by Swami Brahma Muni Parivrajak Vidya Martand 1955 Delhi - Rajpal And Sons.pdf,Savita Devata by Bhagavat Datt Vedalankar 1981 New Delhi - Shri Saraswati Sadan.pdf,Vaisheshik Darshan brahma Muni Bhashyopetam by Swami Brahma Muni Parivrajak 1962 Badowda - Arya Kumar Maha Sabha.pdf,Vedant Darshan with Bhasha Bhashya by Swami Brahma Muni Parivrajak Vidyamartand 2010 Delhi - Satya Dharma Prakashan.pdf,Vedic Yogamrut by Swami Brahma Muni Parivrajak 1949 New Delhi - Sarvadeshik Pustakalay.pdf,Yagnyavalkya Siksha with Hindi Bhashya by Swami Brahma Muni Parivrajak Vidyamartand 1967 Ajmeir - Arya Sahitya Mandal.pdf,Vedant Darshan Brahma Muni Bhashyopetam  by Swami Brahmamuni Parivrajak Vidyamartand 1954 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,Bruhaspati Devata by Bhagavat Datt Vedalankar New Delhi - Shri Saraswati Sadan.pdf,
+Rudra Devata by Bhagavat Datt Vedalankar 1985 Haridwar - Shri Garibdasi Sant Sahitya  Shod Samsthan.pdf,Savita Devata by Bhagavat Datt Vedalankar 1981 New Delhi - Shri Saraswati Sadan.pdf,Ved Vidya Nidarshan by Bhagawat Datt Vedalankar 2005 New Delhi - Pranav Prakashan.pdf,Vedic Adhyatma Vidya by Bhagavat Datt Vedalankar 2007 Haridwar - Gurukul Kangadi Viswavidyalay.pdf,Vedic Swapna Vignyan by Bhagavat Datt Vedalankar 2006 Haridwar - Gurukul Kangadi Viswavidyalay.pdf,Vishnu Devata by Bhagawat Vedalankar 1964 Haridwar - Dharmapal VIdyalankar.pdf,Rigved Par Vyakhyan (Lectures On The Rigveda) by Bhagwadatta 1920 Lahore - Manaoer Vidya Prakasha Press.pdf,Rushi Dayanand Ke Patra Aur Vignapan Part 2 by Bhagawatdatt 1919 Lahore - Devadatt.pdf,Rushi Dayanand Ke Patra Aur Vignyapan Part 1 by Bhagawadatt 1918 Lahore - Bombay Mechine Press.pdf,Rushi Dayanand Ke Patra Aur Vignyapan Part 4 by Bhagawadatt 1917 Punjab - Arya Pradeshik Pratinidhi Sabha.pdf,Yogyata Arthat Rahasyabhed Ka Bhed by Bhagawatdatt 1922 Amrutsar - Arya Yuvak Samaj.pdf
 """
 pdfNames2 = """
-Aryavart By Vaidya Indra Deva 1963 Khamriyapul - Vaidya Indra Dev.pdf,
-Ashtadhyayi By Brahma Datta  1964 Amritsar - Ramlal Kapoor Trust.pdf,
-Ashtadhyayi By Panini 1890 Prayag - Shiva Dayal Sinha.pdf,
-Ashtadhyayi Shukla Yajurveda Prathishakyayoh Mata Vimarsha By Vijay Pal 1983 Haryana - Vijay Pal.pdf,
-Ashtadhyayi Vol-3 By Pandita Brahma Datta - Ramlal Kapoor Trust.pdf,
-Ashtadhyayiya Bhashyam Vol-1 By dayananda Saraswati 1984 Ajmer - Vaidik yantralay.pdf,
-Atha Vedanga Prakasa Vol-8 By Dayananda Saraswati 1960 Ajmer - Vaidik yantralay.pdf,
-Atharva Veda Khanda-20 By Kshema Karan Das Trivedi 1920 Prayag - Shri Raja Rajeshwar.pdf,
-Atharva Veda Samhita With The Commentary of Shankaracharya Vol-2 Edited By Shankar Pandurang 1895 Bombay - Government Central Book Depot.pdf,
-Chromopathy By Dwaraka Nath Narang 1983 New Delhi - Ishwar Das Narang.pdf,
-Fundamental Mysticism In Vedic Worship Or The Mysteries Of Vedic Daily Prayers By Abyudayananda Saraswati 1983 Benaras - Vedodaya Foundation.pdf,
-Kaya Kalpa Yoga By Narendra Nath Shastri 1938 Lucknow - Saraswati Pustak Bhandar.pdf,
-Maha Bhashyam Ch-4 By Patanjali 1889 Benaras - Kripa Ram Sharma.pdf,
-Maha Bhashyam Ch-6 By Patanjali 1889 Benaras - Kripa Ram Sharma.pdf,
-Maha Bhashyam Ch-7 By Patanjali 1889 Benaras - Kripa Ram Sharma.pdf,
-Maha Bhashyam Navahnikam Unidentified Due To Missing Pages - Panini Kanya.pdf,
-Maha Bhashyam Vol-3 With The Commentary Of Kayyata 1889 Benaras - Kripa Ram Sharma.pdf,
-Maha Bhashyam Vol-7 By Sudyumna Acharya 2017 Haryana - Ramlal Kapoor Trust.pdf,
-Maha Bhashyam With The Commentary Of Kayyata Benaras - Kripa Ram Sharma.pdf,
-Nagiri Lipi By Braj Mohan 1968 Benaras - Shabdloka Prakashan.pdf,
-Nunakhar vale Baba Sammohanand By Satyavrat Shastri 2005 Benaras - Shyamala Prakashan.pdf,
-Paniniya Ashtakam Purvardham By Ganga Datta Shastri 1962 Jalandhar - Sat Dharma Pracharak Mandal.pdf,
-Ram Kaleva Benaras - Prithvinath Bhargav.pdf,
-Rigveda Anukramani With The Commentry Of Vijay Pal 1979 Sonipat - Yudhistar.pdf,
-Rigveda Vol-9 Edited By Vishwa Bandhu 1964 Hoshiarpur - Vishweshvaranand Vaidik Sansthan.pdf,
-Samaveda Shatakam Edited By Swami Achyutananda Saraswati Luthiyana - Shanti Ashram.pdf,
-Shri Ram Janma Bhumi Ka Rakta Ranjita Itihas 1998 Ayodhya - Puja Prakashan.pdf,
-Shukla Yajuh Pratishakhyam By Shrimati Indu Rastogi 1967 Benaras - Chaukhamba.pdf,
-Sona Aur Khoon Vol-1 By Acharya Chatursen 1957 Delhi - Rajpal And Sons.pdf,
-Vaidik Adhyatma Jyothi By Anubhavananda Saraswati 1969 New Delhi - Gyan Gyan.pdf,
-Vaidik Vigyan Aur Bharatiya Sanskriti By Giridhar Sharma Chaturvedi 1972 Patna - Bihar Rashtra Basha Parishad.pdf,
-Vaidika Karma Vidhi By Prakasavati Bugga Shastri 1990 New Delhi - Prakasavati Bugga Shastri.pdf,
-Varnochharana Shiksha By Dayananda Saraswati 1950 Ajmer - Vaidika Yantralay.pdf,
-Vayakarana Bhushana Sara By kounda Bhatta Edited By Pandit Taranatha 1872 Calcutta - Ganesha Press.pdf,
-Ved Sarvasva Vol-1 By Hari Prasad Vaidika Muni 1916 Delhi - Sat Dharma Pracharak Mandal.pdf,
-Veda Adhyayan Vol-1 By Ram Prasad 1995 Jwalapur - Shraddha Sahitya Prakashan.pdf,
-Vyakarana Maha Bhashya Of Patanjali Vol-2 Edited By F. Kielhorn 1965 Poona - Bhandarkar Oriental Research Institute.pdf,
-Vyakarana Maha Bhashyam Vol-2 Edited By Veda Vrat 1963 Rohtak - Haryana Sahitya sansthan.pdf,
-Vyakarana Maha Bhashyam Vol-2 Rohtak - Haryana Sahitya Sansthan.pdf,
-Vyakarana Maha Bhashyam with The Commentary Of Kayyata Vol-4 1963 Rohtak - Haryana Sahitya Sansthan.pdf,
-Vyakarana Mahabhashya With Kaiyatas Pradipa Vol-3 By Raghunath Kashinth Shastri 1937 Bombay - Nirnay Sagar Press.pdf,
-Yajurveda Uttarardha - Vajasaneya Samhita Benaras - Gaurish Press.pdf """
+Aadima Satyartha Prakash Aur Arya Samaj Ke Sddhant by Shraddhanand Sanyasi 1918 Karnal - Pandit Vishnu Mitra Sharma.pdf,
+Abhyas Aur Viragya by Brahma Muni Parivrajak Vidyamartand 1961 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,
+Achar Anachar Aur Chut Chat by Munshi Ram Jignyasu 1916 Kangdi - Pracharak Pustak Bhandar.pdf,
+Alankar Tatha Gurukul Samachaar Kangadi - Snatak Gurukul Mandal.pdf,
+Amar Hutatma Ed by Murari 1976 Delhi - Arya Pratinidhi Sabha.pdf,
+Arsha Yoga Pradeepika by Swami Brahma Muni Parivrajak 1949 Dehradun - Pandit Thakur Datt Sharma Trust.pdf,
+Arya Parv Paddati Ajmeir - Arya Sahitya Mandal Limited.pdf,
+Atharva Ved Muni Bhashya by Swami Brahmamuni Parivrajak Vidyamartand 1974 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,
+Atharva Vediya Mantra vidya by Swami Brahmamuni Parivrajak 1941 New Delhi - Dayananda Samsthan.pdf,
+Atharva Vediya Pruthvi Sukth(Matrubhumi Ka Mahatwa by Shri Swami Vedanand Teertha Ajmeir - Arya Sahitya Mandal Limited.pdf,
+Atharvved Muni Bhashya Part 3 by Swami Brahmamuni Parivrajak Vidyamartand 1974 new Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,
+Avvyayartha Nibadhanam by Swami Brahmamuni Parivrajak Vidyamartand 1967 Rohatak - Haryana Sahitya Samsthan.pdf,
+Bala Samskruti Sudha by Swami Brahma Muni Parivrajak Vidyamartand 1961 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,
+Bruhaspati Devata by Bhagavat Datt Vedalankar New Delhi - Shri Saraswati Sadan.pdf,
+Dayand Darshan (Philosaphy Of Swami Dayananda) by Dr.Ved Prakash Gupta 1973 Meerut - Prakashan Pratishtan.pdf,
+Dayand Darshan (Philosaphy Of Swami Dayananda) by Dr.Ved Prakash Gupta 1975 Meerut - Satyartha Prakashan Samsthan.pdf,
+Gurudatt Lekhavali by Gurudatta Ji Vidyarthi Translated by Sant Ram 1918 Lahore - Arya Pustakalay.pdf,
+Kathakam (Die Samhita Der Katha Cakha) by Leopold Von Schroeder 1900 - Anon.pdf,
+Mahabharat Siksha Sudha by Swami Brahma Muni 1953 Ajmeir - Arya Sahitya Mandal.pdf,
+Maharshi Dayanand Ka Jeevan Charitra by Indra Vidya Vachaspati 1950 Delhi - Vijay Pustak Bhandar.pdf,
+Maharshi Dayanand Praneeta Raj Dharma by Lakshmi Datta Deekshit 1950 - Arya Pratinidhi Sabha.pdf,
+Manav Dharma Shastra Tatha Sasana Paddhati by Munshi Ram Jignyasu 1917 Kangadi - Pracharak Pustak Bhandar.pdf,
+Mantrom ke Rushi by Swami Brahma Muni Parivrajak Vidyamartand 1974 Ajmeir - Arya Sahitya Mandal.pdf,
+Mughal Samrajya Ka Kshya Aur Uska Karan Patrt 3 and 4 by Indra Vidya Vachaspati 1936 Mumbai - Hindi Grath Ratnakar Karyalay.pdf,
+Nyaya Darshan with Vatsayana Bhashya and Hindi Translation by Swami Brahma Muni Parivrajak Vidya Martand 1968 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,
+Political Testament Of Swami Shradhanand Punjab - Shri Virendra.pdf,
+Rigved Par Vyakhyan (Lectures On The Rigveda) by Bhagwadatta 1920 Lahore - Manaoer Vidya Prakasha Press.pdf,
+Rudra Devata by Bhagavat Datt Vedalankar 1985 Haridwar - Shri Garibdasi Sant Sahitya Shod Samsthan.pdf,
+Rushi Dayanand Ke Patra Aur Vignapan Part 2 by Bhagawatdatt 1919 Lahore - Devadatt.pdf,
+Rushi Dayanand Ke Patra Aur Vignyapan Part 1 by Bhagawadatt 1918 Lahore - Bombay Mechine Press.pdf,
+Rushi Dayanand Ke Patra Aur Vignyapan Part 4 by Bhagawadatt 1917 Punjab - Arya Pradeshik Pratinidhi Sabha.pdf,
+Samasudha by Swami Brahma Muni Parivrajak Vidya Martand 1963 Delhi - Govindaram Hasanand.pdf,
+Samaved Adhyatmik Muni Bhashya by Swami Brahmamuni Parivrajak Vidya Martand 1972 New Delhi - Arya Pratinidhi Sabha.pdf,
+Samaved Adhyatmik Muni Bhashya by Swami Brahmamuni Parivrajak Vidyamartand 1970 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,
+Samrat Raghu by Indra Vidyavachaspati 1950 Delhi - Vijay Pustak Bhandar.pdf,
+Sankhya Darshanam by Swami Brahma Muni Parivrajak Vidya Martand 1955 Delhi - Rajpal And Sons.pdf,
+Sankhya Darshanam with Brahma Muni Bhashya by Swami Brahma Muni Parivrajak Delhi - Arya Pratinidhi Sabha.pdf,
+Sankshipta Manusmruthi by Munshiram 1909 Kangadi - Saddharma Pracharak Yantralay.pdf,
+Sant Jeevani by Girija Kumar Ghosh 1918 Kangadi - Sahitya Parishad Karyalay.pdf,
+Saraswati Sammelan Ka Chaturtha Varhika Vruttant 1908 Gurukul - Mushiram Jignyasu.pdf,
+Savita Devata by Bhagavat Datt Vedalankar 1981 New Delhi - Shri Saraswati Sadan.pdf,
+Swarajya Aur Charitra Nirman by Indra Vidya vachaspati Delhi - Vachaspati Pustak Bhandar.pdf,
+Terahva Hindi Sahitya Sammelan Kanpur Ka Karya Vivaran Part 1 1923 Cawnpore - Hindi Sahitya Sammelan.pdf,
+Vaisheshik Darshan brahma Muni Bhashyopetam by Swami Brahma Muni Parivrajak 1962 Badowda - Arya Kumar Maha Sabha.pdf,
+Vaisheshik Darshan with Brahma Muni Bhashya by Swami Brahma Muni Parivrajak 1962 Badowda - Arya Kumar Maha Sabha.pdf,
+Ved Vidya Nidarshan by Bhagawat Datt Vedalankar 2005 New Delhi - Pranav Prakashan.pdf,
+Vedadhyayan Praveshika by Swami Brahma Muni Parivrajak Vidyamartand 1968 New Delhi - Sarvadeshika Arya Pratinidhi Sabha.pdf,
+Vedant Darshan Brahma Muni Bhashyopetam by Swami Brahmamuni Parivrajak Vidyamartand 1954 New Delhi - Sarvadeshik Arya Pratinidhi Sabha.pdf,
+Vedant Darshan with Bhasha Bhashya by Swami Brahma Muni Parivrajak Vidyamartand 2010 Delhi - Satya Dharma Prakashan.pdf,
+Vedic Adhyatma Vidya by Bhagavat Datt Vedalankar 2007 Haridwar - Gurukul Kangadi Viswavidyalay.pdf,
+Vedic Swapna Vignyan by Bhagavat Datt Vedalankar 2006 Haridwar - Gurukul Kangadi Viswavidyalay.pdf,
+Vedic Yogamrut by Swami Brahma Muni Parivrajak 1949 New Delhi - Sarvadeshik Pustakalay.pdf,
+Vishnu Devata by Bhagawat Vedalankar 1964 Haridwar - Dharmapal VIdyalankar.pdf,
+Yagnyavalkya Siksha with Hindi Bhashya by Swami Brahma Muni Parivrajak Vidyamartand 1967 Ajmeir - Arya Sahitya Mandal.pdf,
+Yogyata Arthat Rahasyabhed Ka Bhed by Bhagawatdatt 1922 Amrutsar - Arya Yuvak Samaj.pdf """
 
+
+#compare the two lists of pdfs
+pdfNamesAsList = [name.strip() for name in pdfNames.split(',')]
+pdfNamesAsList2 = [name.strip() for name in pdfNames2.split(',')]
+
+# print(pdfNamesAsList)
+# print(pdfNamesAsList2)
+#find the pdfs that are in pdfNamesAsList but not in pdfNamesAsList2 and vice versa
+# pdfNamesAsList = set(pdfNamesAsList)
+# pdfNamesAsList2 = set(pdfNamesAsList2)
+# print length of array
+duplicates = [name for name in set(pdfNamesAsList) if pdfNamesAsList.count(name) > 1]
+print(duplicates)
+
+print( len(pdfNamesAsList) , len(set(pdfNamesAsList)))
+print( len(pdfNamesAsList) , len(set(pdfNamesAsList2)))
+print(set(pdfNamesAsList) - set(pdfNamesAsList2))
