eadb97578c3fc5314c0d518b23d8efe00db92eb6
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2025-06-07 17:12:32 +0530

    Optimizations at Sat06-07-2025_17-12-31.97 ='

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -12,6 +12,6 @@ MONGO_SERVICE_URL = f"http://{MONGO_SERVICE_HOST}:{MONGO_SERVICE_PORT}"
 
 # API endpoints
 API_ENDPOINTS = {
-    'create_entry': f"{MONGO_SERVICE_URL}/createImgToPdfEntry",
-    'update_entry': f"{MONGO_SERVICE_URL}/updateImgToPdfEntry"
+    'create_entry': f"{MONGO_SERVICE_URL}/imgToPdf/createImgToPdfEntry",
+    'update_entry': f"{MONGO_SERVICE_URL}/imgToPdf/updateImgToPdfEntry"
 }
